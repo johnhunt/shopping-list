ad21d315058f5179f5329d31093b05459027e6af
John Hunt <johnrhunt+gh@gmail.com>
2025-03-19 14:18:32 +0000

    Add nix flake to set up dev env, seems to work ok

---
+++ flake.nix
@@ -0,0 +1,62 @@
+{
+description = "Flutter 3.13.x";
+inputs = {
+  nixpkgs.url = "github:NixOS/nixpkgs/23.11";
+  flake-utils.url = "github:numtide/flake-utils";
+};
+outputs = { self, nixpkgs, flake-utils }:
+  flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+        config = {
+          android_sdk.accept_license = true;
+          android_sdk.licensesAccepted = true;  # Extra check
+          allowUnfree = true;
+        };
+      };
+      buildToolsVersion = "34.0.0";
+      androidComposition = pkgs.androidenv.composeAndroidPackages {
+          toolsVersion = "26.1.1";
+          platformToolsVersion = "33.0.3";
+          buildToolsVersions = [ "30.0.3" ];
+          includeEmulator = false;
+          emulatorVersion = "34.1.9";
+          platformVersions = [ "28" "29" "30" "31" ];
+          includeSources = false;
+          includeSystemImages = false;
+          systemImageTypes = [ "google_apis_playstore" ];
+          abiVersions = [ "armeabi-v7a" "arm64-v8a" ];
+          cmakeVersions = [ "3.10.2" ];
+          includeNDK = true;
+          ndkVersions = [ "22.0.7026061" ];
+          useGoogleAPIs = false;
+          useGoogleTVAddOns = false;
+        extraLicenses = [
+          "android-googletv-license"
+          "android-sdk-arm-dbt-license"
+          "android-sdk-license"
+          "android-sdk-preview-license"
+          "google-gdk-license"
+          "intel-android-extra-license"
+          "intel-android-sysimage-license"
+          "mips-android-sysimage-license"            
+       ];
+      };
+      androidSdk = androidComposition.androidsdk;
+    in
+    {
+      devShell =
+        with pkgs; mkShell rec {
+          ANDROID_SDK_ROOT = "${androidSdk}/libexec/android-sdk";
+          buildInputs = [
+            flutter
+            androidSdk # The customized SDK that we've made above
+            jdk17
+          ];
+          postShellHook = ''
+            yes | flutter doctor --android-licenses || true
+          '';
+        };
+    });
+}
